3ceca19ef5603156f19b5479a24604a00f33a27a
mateusz464 <58775549+mateusz464@users.noreply.github.com>
2023-12-22 14:41:26 +0000

    Added Initial API Call

--- Movie Diary/ContentView.swift
+++ Movie Diary/ContentView.swift
@@ -13,17 +13,62 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if viewModel.trending.isEmpty {
+                Text("No results")
+            } else {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(viewModel.trending) {trendingItem in
+                            Text(trendingItem.title)
+                        }
+                    }
+                }
+            }
         }
         .padding()
+        .onAppear {
+            viewModel.loadTrending()
+        }
     }
 }
 
+@MainActor
 class MovieDiaryViewModel: ObservableObject {
-    @Published var homeScreen: []
+    @Published var trending: [TrendingMovies] = []
+    static let apiKey = ProcessInfo.processInfo.environment["TMDB_API_KEY"]
+    
+    func loadTrending() {
+        Task {
+            guard let url = URL(string: "https://api.themoviedb.org/3/trending/movie/week?language=en-US") else {
+                print("Invalid URL")
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.addValue("Bearer \(MovieDiaryViewModel.apiKey!)", forHTTPHeaderField: "Authorization")
+            
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                
+                if !(200..<300 ~= (response as? HTTPURLResponse)?.statusCode ?? 0) {
+                    print("Error getting data")
+                }
+                
+                let trendingResponse = try JSONDecoder().decode(TrendingResults.self, from: data)
+                
+                trending = trendingResponse.results
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+struct TrendingResults: Decodable {
+    let page: Int
+    let results: [TrendingMovies]
+    let total_pages: Int
+    let total_results: Int
 }
 
 struct TrendingMovies: Identifiable, Decodable {
