238ac60f1aa7f4db2b8affccbb412ea74010b104
mateusz464 <58775549+mateusz464@users.noreply.github.com>
2024-01-31 23:25:22 +0000

    Added Views folder

--- Movie Diary/ContentView.swift
+++ Movie Diary/ContentView.swift
@@ -17,8 +17,8 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
 
             VStack {
-                Text("Trending Movies")
-                    .fontWeight(.bold)
+                Text("Trending")
+                    .fontWeight(.heavy)
                     .font(.title)
                     .foregroundColor(.white)
 
@@ -39,38 +39,6 @@ struct ContentView: View {
                 .onAppear {
                     viewModel.loadTrending()
                 }
-            }
-        }
-    }
-}
-
-@MainActor
-class MovieDiaryViewModel: ObservableObject {
-    @Published var trending: [TrendingMovies] = []
-    static let apiKey = ProcessInfo.processInfo.environment["TMDB_API_KEY"]
-    
-    func loadTrending() {
-        Task {
-            guard let url = URL(string: "https://api.themoviedb.org/3/trending/movie/week?language=en-US") else {
-                print("Invalid URL")
-                return
-            }
-            
-            var request = URLRequest(url: url)
-            request.addValue("Bearer \(MovieDiaryViewModel.apiKey!)", forHTTPHeaderField: "Authorization")
-            
-            do {
-                let (data, response) = try await URLSession.shared.data(for: request)
-                
-                if !(200..<300 ~= (response as? HTTPURLResponse)?.statusCode ?? 0) {
-                    print("Error getting data")
-                }
-                
-                let trendingResponse = try JSONDecoder().decode(TrendingResults.self, from: data)
-                
-                trending = trendingResponse.results
-            } catch {
-                print(error.localizedDescription)
             }
         }
     }
