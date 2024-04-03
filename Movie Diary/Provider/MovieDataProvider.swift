27bf04a0e89d5cf4d0b3f536a8fca45a4854a84f
mateusz464 <58775549+mateusz464@users.noreply.github.com>
2024-04-03 14:49:53 +0100

    Added MovieDataProvider

---
+++ Movie Diary/Provider/MovieDataProvider.swift
@@ -0,0 +1,29 @@
+//
+//  WatchedProvider.swift
+//  Movie Diary
+//
+//  Created by Mateusz Golebiowski on 03/04/2024.
+//
+
+import Foundation
+import CoreData
+
+final class MovieDataProvider {
+    static let shared = MovieDataProvider()
+    
+    private let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "MovieDiaryData")
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+        persistentContainer.loadPersistentStores { _, error in
+            if let error {
+                fatalError("Unable to load store with error: \(error)")
+            }
+        }
+    }
+}
